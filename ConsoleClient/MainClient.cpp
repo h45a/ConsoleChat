62165dae35aeccb1b1df3488bbe268f40aed526d
5308-4 <5308-4@DESKTOP-H9VD3KL>
2024-12-10 13:34:45 +0400

    Add project files.

---
+++ ConsoleClient/MainClient.cpp
@@ -0,0 +1,58 @@
+﻿/*
+    UDP Client routine
+*/
+#include "UDPSocket.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ws2tcpip.h> // Для inet_pton
+
+int main(int argc, char* argv[]) {
+    struct sockaddr_in si_other;
+    int slen = sizeof(si_other);
+    char buf[BUFLEN];
+    char message[BUFLEN];
+
+    unsigned short srv_port = 0;
+    char srv_ip_addr[40] = { 0 };
+
+    // Создаём UDP сокет
+    UDPSocket client_sock;
+
+    // Настраиваем адрес сервера
+    memset((char*)&si_other, 0, sizeof(si_other));
+    si_other.sin_family = AF_INET;
+
+    if (argc == 1) {
+        si_other.sin_port = htons(PORT);
+        inet_pton(AF_INET, SERVER, &si_other.sin_addr); // Используем inet_pton вместо inet_addr
+        printf("1: Server - addr=%s , port=%d\n", SERVER, PORT);
+    }
+    else if (argc == 2) {
+        si_other.sin_port = htons(atoi(argv[1]));
+        inet_pton(AF_INET, SERVER, &si_other.sin_addr);
+        printf("2: Server - addr=%s , port=%d\n", SERVER, atoi(argv[1]));
+    }
+    else {
+        si_other.sin_port = htons(atoi(argv[2]));
+        inet_pton(AF_INET, argv[1], &si_other.sin_addr);
+        printf("3: Server - addr=%s , port=%d\n", argv[1], atoi(argv[2]));
+    }
+
+    // Основной цикл отправки сообщений
+    while (1) {
+        printf("\nEnter message: ");
+        gets_s(message, BUFLEN);
+
+        // Отправляем сообщение серверу
+        client_sock.SendDatagram(message, (int)strlen(message), (struct sockaddr*)&si_other, slen);
+
+        // Получаем ответ от сервера
+        memset(buf, '\0', BUFLEN);
+        client_sock.RecvDatagram(buf, BUFLEN, (struct sockaddr*)&si_other, &slen);
+
+        printf("Answer: %s\n", buf); // Добавлено "Answer:" перед ответом
+    }
+
+    return 0;
+}
---
+++ ConsoleServer/MainServer.cpp
@@ -0,0 +1,49 @@
+﻿/*
+    UDP Server
+*/
+#include "UDPServer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ws2tcpip.h> // Для inet_ntop
+
+int main(int argc, char* argv[]) {
+    struct sockaddr_in si_other;
+    unsigned short srvport;
+    int slen;
+    char buf[BUFLEN];
+    char msg[BUFLEN];
+    char client_ip[INET_ADDRSTRLEN]; // Для хранения IP-адреса клиента
+
+    srvport = (argc == 1) ? PORT : atoi(argv[1]);
+
+    // Создаём UDP сервер
+    UDPServer server(srvport);
+    slen = sizeof(si_other);
+
+    // Основной цикл обработки запросов
+    while (1) {
+        printf("Waiting for data... ");
+        fflush(stdout);
+
+        // Очищаем буфер
+        memset(buf, '\0', BUFLEN);
+
+        // Получаем сообщение от клиента
+        server.RecvDatagram(buf, BUFLEN, (struct sockaddr*)&si_other, &slen);
+
+        // Преобразуем IP-адрес клиента в строку
+        inet_ntop(AF_INET, &si_other.sin_addr, client_ip, INET_ADDRSTRLEN);
+
+        // Выводим данные клиента
+        printf("\nReceived packet from %s:%d\n", client_ip, ntohs(si_other.sin_port));
+        printf("Data: %s\n", buf);
+
+        // Отвечаем клиенту
+        printf("Answer: ");
+        gets_s(msg, BUFLEN);
+        server.SendDatagram(msg, (int)strlen(msg), (struct sockaddr*)&si_other, slen);
+    }
+
+    return 0;
+}
